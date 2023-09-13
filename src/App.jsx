4a2e7ac364a5a1a4605de0fb6f26e52276ac4d88
bismarkboateng <bismarkb609@gmail.com>
2023-09-13 22:29:55 +0000

    movie detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { Navbar, DescriptionBox, MovieList } from "./components"
+import { Navbar, DescriptionBox, MovieList, MovieDetail} from "./components"
 import './App.css'
 import { Poster } from "./assets"
+import { Routes, Route} from "react-router-dom"
 
 
 function App() {
 
   return (
-    <div>
-      <img
-        src={Poster}
-        className="absolute top-0 left-0 z-[-10] h-[600px] w-[1440px]"
-      />
-      <Navbar />
-      <DescriptionBox />
-      
-      <main className="mt-[15%]">
-        <MovieList />
-      </main>
-      
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/movie/:movieId" element={<MovieDetail /> } />
+      </Routes>
+      <div>
+        <img
+          src={Poster}
+          className="absolute top-0 left-0 z-[-10] h-[600px] w-[1440px]"
+        />
+        <Navbar />
+        <DescriptionBox />
+        
+        <main className="mt-[15%]">
+          <MovieList />
+        </main>
+      </div>
+    </>
   )
 }
 
--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,25 +1,31 @@
-import { Movie_Image } from "../../assets"
 import { Rating } from ".."
+import { Link } from "react-router-dom"
+
 
 export default function index({ movie }) {
   console.log(movie)
-  
-  const genre="Action, Adventure, Drama"
+
+  const genre = "Actions, Adventures, fiction"
 
   return (
     <div className="w-[250px] h-[490px]">
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
-        alt="movie"
-      />
-      <div className="text-gray-400 font-bold text-xs leading-[15.62px] mt-3">USA, 2016 - Current</div>
-      <div className="text-gray-700 font-bold text-lg leading-[23.44px] mt-3 mb-3">Stranger Things</div>
-      <Rating
-        rating="86.0 / 100"
-        like="97%"
-        id={1}
-      />
-      <div className="font-bold text-xs leading-[15.62px] text-gray-400">{genre}</div>
-    </div>
+        <Link
+          to={`/movie/${movie?.id}`}
+          style={{ textDecoration: "none" }}
+        >
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+          alt="movie"
+        />
+        </Link>
+        <div className="text-gray-400 font-bold text-xs leading-[15.62px] mt-3">USA, 2016 - Current</div>
+        <div className="text-gray-700 font-bold text-lg leading-[23.44px] mt-3 mb-3">{movie?.original_title}</div>
+        <Rating
+          rating="86.0 / 100"
+          like="97%"
+          id={movie?.id}
+        />
+        <div className="font-bold text-xs leading-[15.62px] text-gray-400">{genre}</div>
+      </div>
   )
 }
